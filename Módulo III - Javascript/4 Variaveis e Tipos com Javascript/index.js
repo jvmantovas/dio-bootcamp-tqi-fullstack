a5e85b16d9060af580d510c61f48ba84b163cd28
jvmantovas <vmsjoao@gmail.com>
2022-07-19 00:51:26 -0300

    update

---
+++ Módulo III - Javascript/4 Variaveis e Tipos com Javascript/index.js
@@ -0,0 +1,16 @@
+/* var firstName = 'João';
+var lastName = 'Mantovas';
+
+if(firstName === 'João') {
+    var firstName = 'Isa';
+    let lastName = 'Terçariol';
+
+}
+
+console.log(firstName, lastName);
+
+ */
+
+const FIRST_NAME = 'jonas'
+
+console.log(FIRST_NAME);
